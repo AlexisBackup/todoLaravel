3d4e1e101c07d7932ed4ead8150e7d01f65ce0c2
Alexis Mathiot <alexis.mathiot.com>
2025-05-09 14:08:46 +0200

    refactor post and edit task

--- src/app/Http/Controllers/TaskController.php
+++ src/app/Http/Controllers/TaskController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\TaskRequest;
 use App\Models\Task;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
@@ -14,26 +15,42 @@ class TaskController extends Controller
         return view('index')->with('tasks', Task::latest()->get());
     }
 
-    public function getById($id): View
+    public function getById(Task $task): View
     {
-        return view('show', ['task' => Task::findOrFail($id)]);
+        return view('show', ['task' => $task]);
     }
 
-    public function store(Request $request): RedirectResponse
+    public function store(TaskRequest $request): RedirectResponse
     {
-        $data = $request->validate([
-            'title' => 'required | max:255',
-            'description' => 'required',
-            'long_description' => 'required'
-        ]);
+        // $data = $request->validated();
 
-        $task = new Task();
-        $task->title = $data['title'];
-        $task->description = $data['description'];
-        $task->long_description = $data['long_description'];
+        // $task = new Task();
+        // $task->title = $data['title'];
+        // $task->description = $data['description'];
+        // $task->long_description = $data['long_description'];
 
-        $task->save();
+        // $task->save();
+        $task = Task::create($request->validated());
 
-        return redirect()->route('tasks.show', ['id' => $task->id]);
+        return redirect()->route('tasks.show', ['task' => $task->id])->with('success', 'Task create successfully !');
+    }
+
+    public function editShow(Task $task): View
+    {
+        return view('edit', ['task' => $task]);
+    }
+
+    public function editHandle(Task $task, TaskRequest $request): RedirectResponse
+    {
+        // $data = $request->validated();
+
+        // $task->title = $data['title'];
+        // $task->description = $data['description'];
+        // $task->long_description = $data['long_description'];
+
+        // $task->save();
+        $task->update($request->validated());
+
+        return redirect()->route('tasks.show', ['task' => $task->id])->with('success', 'Task updated successfully !');
     }
 }
--- src/routes/web.php
+++ src/routes/web.php
@@ -11,8 +11,12 @@ Route::get('/', function () {
 
 Route::view('/tasks/create', 'create')->name('tasks.create');
 
-Route::get('/tasks/{id}', [TaskController::class, 'getById'])->name('tasks.show');
+Route::get('/tasks/{task}', [TaskController::class, 'getById'])->name('tasks.show');
 
 Route::get('/tasks', [TaskController::class, 'getAll'])->name('tasks.index');
 
 Route::post('/tasks', [TaskController::class, 'store'])->name('tasks.store');
+
+Route::get('/tasks/{task}/edit', [TaskController::class, 'editShow'])->name('tasks.edit.show');
+
+Route::put('/tasks/{task}', [TaskController::class, 'editHandle'])->name('tasks.edit.handle');
